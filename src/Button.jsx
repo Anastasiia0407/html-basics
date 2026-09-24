7d01bd837a43d0623564f6882594c3d50dad169f
Anastasiia0407 <anastasiia.yevtodii@gmail.com>
2026-09-24 13:31:46 +0100

    Migrate button to Tailwind

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,3 +1,11 @@
+const CLASSES = [
+  "w-full",
+  "font-sans text-sm text-white",
+  "bg-pink-700 hover:bg-pink-800",
+  "border border-solid border-pink-700 hover:border-pink-800",
+  "rounded-lg p-2 cursor-pointer",
+].join(" ");
+
 export default function Button({ children }) {
-  return <button className="button">{children}</button>;
+  return <button className={CLASSES}>{children}</button>;
 }
